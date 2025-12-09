f446265d53dbae7e51d88fe57dac144e962d3d76
drwho495 <drwho495555@gmail.com>
2025-12-09 16:31:26 -0600

    implement base fix

--- src/App/PropertyLinks.cpp
+++ src/App/PropertyLinks.cpp
@@ -420,6 +420,7 @@ bool PropertyLinkBase::_updateElementReference(DocumentObject* feature,
                                                bool reverse,
                                                bool notify)
 {
+    FC_WARN("update reverse: " << reverse);
     if (!obj || !obj->getNameInDocument()) {
         return false;
     }
@@ -480,7 +481,11 @@ bool PropertyLinkBase::_updateElementReference(DocumentObject* feature,
             if (names.size()) {
                 missing = false;
                 std::string newsub(subname, strlen(subname) - strlen(element));
-                newsub += names.front();
+                if (names.size()) {
+                    newsub += names.front();
+                } else if (reverse) {
+                    newsub += oldElement;
+                }
                 GeoFeature::resolveElement(obj,
                                            newsub.c_str(),
                                            elementName,
@@ -495,6 +500,8 @@ bool PropertyLinkBase::_updateElementReference(DocumentObject* feature,
                            << " auto change element reference " << ret->getFullName() << " "
                            << oldName << " -> " << newName);
                 }
+            } else if (reverse) {
+                FC_WARN("not found in cache");
             }
         }
     }
